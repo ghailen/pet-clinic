d64d7682dd0f5ea462cee4e0b7d32c25bc7f81e3
ghailen <ghailenemark@gmail.com>
2021-08-19 15:30:59 +0100

    add controllers

---
+++ pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.ghailen.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+
+}
