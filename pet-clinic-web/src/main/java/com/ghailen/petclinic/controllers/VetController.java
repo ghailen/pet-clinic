045c463ba816d204c66250f5c67a5d7389096484
ghailen <ghailenemark@gmail.com>
2021-08-19 15:47:10 +0100

    add controller owner

--- pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/VetController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","vets/index","/vets/index.html"})
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
     public String listVets() {
         return "vet/index";
     }
