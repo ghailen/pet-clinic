d537f4e59991c7e6977be9e2e2401de95adec4fa
ghailen <ghailenemark@gmail.com>
2021-08-25 16:26:45 +0100

    refactor id auto generated

--- pet-clinic-data/src/main/java/com/ghailen/petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/ghailen/petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.ghailen.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.ghailen.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends  Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
 
     Set<T> findAll() {
@@ -19,8 +18,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object !=null){
+            if(object.getId()==null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }else {
+            throw new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
 
@@ -30,5 +37,15 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId(){
+        Long nextId = null;
+        try{
+            nextId =Collections.max(map.keySet()) +1;;
+        }catch (NoSuchElementException e){
+            nextId=1L;
+        }
+       return nextId;
     }
 }
