6c069d5c1447762a3f8c9217fb2a8e16bea45551
ghailen <ghailenemark@gmail.com>
2021-08-25 16:14:43 +0100

    list vets

--- pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.ghailen.petclinic.controllers;
 
+import com.ghailen.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets",vetService.findAll());
+
         return "vet/index";
     }
 
