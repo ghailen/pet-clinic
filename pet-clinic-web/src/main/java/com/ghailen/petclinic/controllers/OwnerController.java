061700c5a266c08137a19cc1abc95b04fa3ce6db
ghailen <ghailenemark@gmail.com>
2021-08-25 16:11:29 +0100

    list owners

--- pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/OwnerController.java
@@ -1,14 +1,22 @@
 package com.ghailen.petclinic.controllers;
 
+import com.ghailen.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
 
-    @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 
