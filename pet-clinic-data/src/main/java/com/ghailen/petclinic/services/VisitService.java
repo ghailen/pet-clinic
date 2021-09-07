a43ef0abceebd06daa68e4afe165f4de192c9f84
ghailen <ghailenemark@gmail.com>
2021-09-08 00:33:45 +0100

    add visit map service

---
+++ pet-clinic-data/src/main/java/com/ghailen/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.ghailen.petclinic.services;
+
+import com.ghailen.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/ghailen/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package com.ghailen.petclinic.services.map;
+
+import com.ghailen.petclinic.model.Owner;
+import com.ghailen.petclinic.model.Visit;
+import com.ghailen.petclinic.services.OwnerService;
+import com.ghailen.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findByID(aLong);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+}
