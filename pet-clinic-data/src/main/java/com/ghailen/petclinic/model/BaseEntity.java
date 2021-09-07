c9318bfd0a1359d6d61efaffe824e81b6244a0ef
ghailen <ghailenemark@gmail.com>
2021-09-07 22:59:09 +0100

    add base entity entity

--- pet-clinic-data/src/main/java/com/ghailen/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ghailen/petclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package com.ghailen.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// other class will herit from this class
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
