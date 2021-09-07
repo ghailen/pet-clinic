2707af523d8aeecfca54864572797a5d5fcf0c8c
ghailen <ghailenemark@gmail.com>
2021-09-08 00:11:44 +0100

    spring data jpa for pet type and vet

---
+++ pet-clinic-data/src/main/java/com/ghailen/petclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,45 @@
+package com.ghailen.petclinic.services.springdatajpa;
+
+import com.ghailen.petclinic.model.PetType;
+import com.ghailen.petclinic.repositories.PetTypeRepository;
+import com.ghailen.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/ghailen/petclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,45 @@
+package com.ghailen.petclinic.services.springdatajpa;
+
+import com.ghailen.petclinic.model.Vet;
+import com.ghailen.petclinic.repositories.VetRepository;
+import com.ghailen.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
