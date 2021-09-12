9971508aec24b9f33696f5c8b57e660b2d745f21
ghailen <ghailenemark@gmail.com>
2021-09-13 00:01:02 +0100

    add pass icon to readme

--- pet-clinic-data/src/main/java/com/ghailen/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ghailen/petclinic/model/Pet.java
@@ -6,30 +6,45 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
-@Table(name="pets")
+@Table(name = "pets")
 public class Pet extends BaseEntity {
-    @Column(name="name")
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        if (visits == null || visits.size() > 0) {
+            this.visits = visits;
+        }
+
+    }
+
+
+    @Column(name = "name")
     private String name;
 
 
     @ManyToOne
-    @JoinColumn(name="type_id")
+    @JoinColumn(name = "type_id")
     private PetType petType;
 
     @ManyToOne
-    @JoinColumn(name="owner_id")
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
-    @Column(name="birth_date")
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
-    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
 
 }
---
+++ pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/VisitController.java
@@ -0,0 +1,67 @@
+package com.ghailen.petclinic.controllers;
+
+import com.ghailen.petclinic.model.Pet;
+import com.ghailen.petclinic.model.Visit;
+import com.ghailen.petclinic.services.PetService;
+import com.ghailen.petclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+public class VisitController {
+
+    private final VisitService visitService;
+
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    /**
+     * Called before each and every @RequestMapping annotated method. 2 goals: - Make sure
+     * we always have fresh data - Since we do not use the session scope, make sure that
+     * Pet object always has an id (Even though id is not part of the form fields)
+     *
+     * @param petId
+     * @return Pet
+     */
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Model model) {
+        Pet pet = petService.findById(petId);
+        // pet.setVisitsInternal(visitRepository.findByPetId(petId));
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        return visit;
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before initNewVisitForm is called
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable("petId") int petId, Map<String, Object> model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
