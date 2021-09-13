151d598af07a6dec67366dda054651277016bcd3
ghailen <ghailenemark@gmail.com>
2021-09-13 14:46:17 +0100

    response body json to vet controller

--- pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ghailen/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.ghailen.petclinic.controllers;
 
+import com.ghailen.petclinic.model.Vet;
 import com.ghailen.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -14,12 +19,21 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
 
-        model.addAttribute("vets",vetService.findAll());
+        model.addAttribute("vets", vetService.findAll());
 
         return "vet/index";
     }
+
+    // response body json
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
+
+    }
+
 
 }
