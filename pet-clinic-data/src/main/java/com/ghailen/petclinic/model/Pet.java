2d275a79f162547f46c5d65706a4a972a6b09403
ghailen <ghailenemark@gmail.com>
2021-09-13 14:42:44 +0100

    add speciality to vet

--- pet-clinic-data/src/main/java/com/ghailen/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ghailen/petclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.ghailen.petclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -42,6 +43,7 @@ public class Pet extends BaseEntity {
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
